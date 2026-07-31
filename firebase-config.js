512efcd777b28fe18aea3c017a63b2e62797865f
agostinocambareri2-max <agostinocambareri2@gmail.com>
2026-07-31 18:49:25 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+/* Configurazione Firebase pubblica dell'app Web MAGAZZINO LAB. */
+window.FIREBASE_CONFIG = Object.freeze({
+  apiKey: "AIzaSyC2780dqcVNbNX1qjVlbY6QExiK5tDCYqA",
+  authDomain: "magazzino-lab.firebaseapp.com",
+  projectId: "magazzino-lab",
+  storageBucket: "magazzino-lab.firebasestorage.app",
+  messagingSenderId: "128772354672",
+  appId: "1:128772354672:web:7835c49b9c756b7d69bab5"
+});
